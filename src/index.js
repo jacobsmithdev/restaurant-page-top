eeb918dfb18e5fed97cb01d13ad09039c7d40cce
Jacob Smith <129335957+jsmith-code@users.noreply.github.com>
2025-03-31 15:16:01 -0600

    feat: Load aboutPageContent when about button is clicked

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './styles.css';
 import homePageContent from './modules/homePage.js'
 import menuPageContent from './modules/menuPage.js';
+import aboutPageContent from './modules/aboutPage.js';
 
 const pageContent = document.querySelector('#content')
 
 const homeBtn = document.querySelector('#home');
 const menuBtn = document.querySelector('#menu');
+const aboutBtn = document.querySelector('#about');
 
 function setPageContent(content) {
     pageContent.textContent = '';
@@ -18,6 +20,10 @@ homeBtn.addEventListener('click', () => {
 
 menuBtn.addEventListener('click', () => {
     setPageContent(menuPageContent);
+});
+
+aboutBtn.addEventListener('click', () => {
+    setPageContent(aboutPageContent);
 });
 
 setPageContent(homePageContent);
