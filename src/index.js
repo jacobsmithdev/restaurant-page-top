b857982ef1d8a804d44d5d878a0715be441656ef
Jacob Smith <129335957+jsmith-code@users.noreply.github.com>
2025-03-31 14:55:35 -0600

    feat: Load homePageContent when home button is clicked

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import homePageContent from './modules/homePage.js'
 
 const pageContent = document.querySelector('#content')
 
+const homeBtn = document.querySelector('#home');
+
 function setPageContent(content) {
     pageContent.textContent = '';
     pageContent.append(content);
 }
+
+homeBtn.addEventListener('click', () => {
+    setPageContent(homePageContent);
+});
 
 setPageContent(homePageContent);
