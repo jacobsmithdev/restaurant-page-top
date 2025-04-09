8f1ce6490b29db3c5143800df582385baf3cd686
Jacob Smith <129335957+jsmith-code@users.noreply.github.com>
2025-04-09 16:10:48 -0600

    feat: Add content for menu page

--- src/modules/menuPage.js
+++ src/modules/menuPage.js
@@ -1,6 +1,29 @@
-const menuPage = document.createElement('div');
 
-menuPage.textContent = 'this is the menu page';
-menuPage.classList.add('menu');
+const menuOptions = ["Specials", "Meals", "Breads", "Sides", "Drinks"];
+
+function createMenuElement() {
+    const menu = document.createElement('div');
+    const header = document.createElement('h1');
+    const foodList = document.createElement('ul')
+
+    menu.classList.add('menu');
+    header.classList.add('menu__header');
+    foodList.classList.add('menu-list');
+    
+    header.textContent = 'Menu';
+
+    menuOptions.forEach(option => {
+        const item = document.createElement('li')
+        item.classList.add('menu-list__item');
+        item.textContent = option;
+        foodList.append(item);
+    })
+
+    menu.append(header, foodList);
+
+    return menu;
+}
+
+const menuPage = createMenuElement();
 
 export default menuPage;
