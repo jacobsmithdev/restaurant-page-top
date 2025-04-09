adec1fe329676e1d4eef839243a8812ca810da2a
Jacob Smith <129335957+jsmith-code@users.noreply.github.com>
2025-04-09 16:30:07 -0600

    feat: Add content for about page

--- src/modules/aboutPage.js
+++ src/modules/aboutPage.js
@@ -1,6 +1,46 @@
-const aboutPage = document.createElement('div');
+const aboutListItems = [
+    "Contact", 
+    "Privacy Policy", 
+    "Copyright info", 
+    "See all locations", 
+    "Hiring"
+];
 
-aboutPage.textContent = 'this is the about page';
-aboutPage.classList.add('about');
+const aboutTextContent = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, velit est voluptate voluptatibus alias aut! Quasi, natus. Assumenda, repellendus aspernatur. Nam sequi corporis distinctio molestias voluptatibus vero incidunt repudiandae deleniti!"
+
+function createMenuElement() {
+    const about = document.createElement('div');
+    const header = document.createElement('h1');
+    const aboutText = document.createElement('p');
+    const aboutList = document.createElement('ul');
+
+    about.classList.add('about');
+    header.classList.add('about__header');
+    aboutList.classList.add('about__list');
+    
+    header.textContent = 'About';
+    aboutText.textContent = aboutTextContent;
+
+    aboutListItems.forEach(option => {
+        const item = document.createElement('li')
+        const link = document.createElement('a');
+
+        item.classList.add('about__list-item');
+        link.classList.add('about__list-link')
+
+        link.textContent = option;
+
+        link.setAttribute('href', '#');
+        
+        item.append(link);
+        aboutList.append(item);
+    })
+
+    about.append(header, aboutText, aboutList);
+
+    return about;
+}
+
+const aboutPage = createMenuElement();
 
 export default aboutPage;
