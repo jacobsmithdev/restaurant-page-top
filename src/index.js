604eb1221e37593c513c393c9baa26958acb8e50
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-04-17 12:19:38 -0600

    feat(css): Add highlight to header button matching display content
    Add styling to higlight the header button corresponding to the page's
    content (e.g., if 'home page' is displayed, highlight the 'home' button
    in the header).

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import aboutPageContent from './modules/aboutPage.js';
 
 const pageContent = document.querySelector('#content')
 
+const headerBtnsContainer = document.querySelector('.header__nav');
+
 const homeBtn = document.querySelector('#home');
 const menuBtn = document.querySelector('#menu');
 const aboutBtn = document.querySelector('#about');
@@ -14,16 +16,26 @@ function setPageContent(content) {
     pageContent.append(content);
 }
 
+function setHeaderBtnFocus(targetBtn) {
+    const headerBtns = Array.from(headerBtnsContainer.children);
+    headerBtns.forEach(btn => btn.classList.remove('button--header--selected'))
+    targetBtn.classList.add('button--header--selected');
+}
+
 homeBtn.addEventListener('click', () => {
     setPageContent(homePageContent);
+    setHeaderBtnFocus(homeBtn);
 });
 
 menuBtn.addEventListener('click', () => {
     setPageContent(menuPageContent);
+    setHeaderBtnFocus(menuBtn);
 });
 
 aboutBtn.addEventListener('click', () => {
     setPageContent(aboutPageContent);
+    setHeaderBtnFocus(aboutBtn);
 });
 
 setPageContent(homePageContent);
+setHeaderBtnFocus(homeBtn);
