00a5858e70172a093eab094538f3535e36255c42
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-07-28 13:18:15 -0600

    feat: Restyle menu page to match the rest of the project

--- src/modules/menuPage.js
+++ src/modules/menuPage.js
@@ -4,14 +4,29 @@ const menuOptions = ["Specials", "Meals", "Breads", "Sides", "Drinks"];
 
 function createMenuElement() {
     const menu = document.createElement('div');
+
     const header = document.createElement('h1');
+    const headerOuterBorder = document.createElement('div');
+    const headerInnerBorder = document.createElement('div');
+
     const foodList = document.createElement('ul')
+    const foodListWrapper = document.createElement('div');
 
     menu.classList.add('menu');
+
     header.classList.add('menu__header');
+    headerOuterBorder.classList.add('framed-border__outer')
+    headerInnerBorder.classList.add('framed-border__inner')
+
+    foodListWrapper.classList.add('framed-border__outer')
+
     foodList.classList.add('menu-list');
     
     header.textContent = 'Menu';
+
+    foodListWrapper.append(foodList);
+    headerInnerBorder.append(header);
+    headerOuterBorder.append(headerInnerBorder);
 
     menuOptions.forEach(option => {
         const item = document.createElement('li')
@@ -20,7 +35,7 @@ function createMenuElement() {
         foodList.append(item);
     })
 
-    menu.append(header, foodList);
+    menu.append(headerOuterBorder, foodListWrapper);
 
     return menu;
 }
